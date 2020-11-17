9bf505acbe46b2a3dc00a1cbe8c0e3e3e03dbfb5
anawad <anawad@umich.edu>
2020-11-16 21:54:06 -0500

    added notes functionality

--- src/Components/Dash.jsx
+++ src/Components/Dash.jsx
@@ -13,19 +13,35 @@ class Card extends Component {
   }
 }
 
+
+class Note extends Component {
+  render() {
+    return(
+      <div class="note">
+        <div class="note-title">{this.props.title}</div>
+      </div>
+    )
+  }
+}
+
+
+
 export default class Dash extends Component {
   render() {
     return (
       <div>
         <h1>Welcome to your dashboard!</h1>
         <div class="card-cont">
-          <Card title="Happiness" num="9.7" subtitle="this week" />
-          <Card title="Health" num="4.7" subtitle="this week" />
-          <Card title="????" num="4.7" subtitle="this week" />
-          <Card title="????" num="4.7" subtitle="this week" />
-          <Card title="????" num="4.7" subtitle="this week" />
-          <Card title="????" num="4.7" subtitle="this week" />
+          <Card title="🔥 STREAK" num="6 days in a row" subtitle="" />
+          <Card title="😃 HAPPINESS" num="9.7" subtitle="this week" />
+          <Card title="💪 HEALTH" num="4.7" subtitle="this week" />
+          <Card title="✍️ PRODUCTIVITY" num="4.7" subtitle="this week" />
+
         </div>
+        <Note title="this is a test note" />
+        <Note title="this is a second test note" />
+
+        
 
         <div>
           <div class="plus-button">
