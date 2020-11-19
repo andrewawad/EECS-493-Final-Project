cf5996d19a8f9c36f9fb645a82d4a6e0fa8edf3b
nmtan <nmtan@umich.edu>
2020-11-19 17:41:29 -0500

    adf

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Slider from 'react-rangeslider';
-import 'react-rangeslider/lib/index.css';
-var Slider = require('react-rangeslider');
 import '../style.css';
 
 
@@ -10,18 +8,19 @@ export default class Form extends Component {
     constructor(props) {
         super(props)
         this.state = {
-          rating: 6
+            productive: 5
         }
-    }
-     
-    handleOnChange = (value) => {
-        this.setState({
-            rating: value
-        })
+
+        this.handleChange = this.handleChange.bind(this);
     }
     
+    handleChange(event) {
+        this.setState({productive: event.target.value});
+      }
+    
     render() {
-      let { rating } = this.state;
+      let { rating, productive } = this.state;
+
 
       return (
         <div>
@@ -31,28 +30,24 @@ export default class Form extends Component {
 
             <h3 class="formQuestion">How productive were you?</h3>
 
-            <Slider value={rating} orientation="horizontal" onChange={this.handleOnChange}/>
-
-            {/* <div class="slidecontainer">
-                <input type="range" min="1" max="10" value="6" class="slider" id="myRange">
-                </input>
+            <div class="slidecontainer">
+                <input type="range" min="1" max="10" value={productive} onChange={this.handleChange} class="slider"></input>
+                <p>{productive}</p>
             </div>
 
             <h3 class="formQuestion">How well did you handle stress?</h3>
 
             <div class="slidecontainer">
-                <input type="range" min="1" max="10" value="6" class="slider" id="myRange">
-                </input>
+            <input type="range" min="1" max="10" value="5" class="slider"></input>
             </div>
 
             <h3 class="formQuestion">How happy were you?</h3>
 
             <div class="slidecontainer">
-                <input type="range" min="1" max="10" value="6" class="slider" id="myRange">
-                </input>
+                <input type="range" min="1" max="10" value="5" class="slider"></input>
             </div>
 
-            <h3 class="formQuestion">Today's Journal</h3> */}
+            <h3 class="formQuestion">Today's Journal</h3>
 
 
 
