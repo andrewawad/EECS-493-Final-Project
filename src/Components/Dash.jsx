a26b5d1cae755b4ac9c7bf65629e5d76324368ad
nmtan <nmtan@umich.edu>
2020-12-03 22:36:14 -0500

    asdf

--- src/Components/Dash.jsx
+++ src/Components/Dash.jsx
@@ -4,99 +4,77 @@ import {
   Switch,
   Route,
   Redirect,
-  Link
+  Link,
 } from "react-router-dom";
 import "../style.css";
-import {authentication} from "./services/firebase";
-import firebase from 'firebase';
+import { authentication } from "./services/firebase";
+import firebase from "firebase";
 import "firebase/auth";
 import "firebase/firestore";
-import ReactCardFlip from 'react-card-flip';
-import {XYPlot, XAxis, YAxis,   VerticalGridLines,    HorizontalGridLines, LineSeries,  AreaSeries, GradientDefs,  Borders
+import ReactCardFlip from "react-card-flip";
+// import {
+//   XYPlot,
+//   XAxis,
+//   YAxis,
+//   VerticalGridLines,
+//   HorizontalGridLines,
+//   LineSeries,
+//   AreaSeries,
+//   GradientDefs,
+//   Borders,
+// } from "react-vis";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
 
-} from 'react-vis';
 
-const DAYS = [
-  'Mon',
-  'Tu',
-  'Wed ',
-  'Thu ',
-  'Fri',
-  'Sat',
-  'Sun',
-  ];
+const DAYS = ["Mon", "Tu", "Wed ", "Thu ", "Fri", "Sat", "Sun"];
 class Card extends Component {
   constructor() {
     super();
-      this.state = {
-      isFlipped: false
+    this.state = {
+      isFlipped: false,
     };
     this.handleClick = this.handleClick.bind(this);
   }
- 
+
   handleClick(e) {
     e.preventDefault();
-    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
+    this.setState((prevState) => ({ isFlipped: !prevState.isFlipped }));
   }
 
   render() {
-    
+    // console.log(this.props.data)
     return (
-      <ReactCardFlip isFlipped={this.state.isFlipped} infinite>
-        <div class="card" onClick={this.handleClick}>
-          <h2 class="card-title">{this.props.title}</h2>
-          <p class="card-num">{this.props.num}</p>
-          <h3 class="card-subtitle">{this.props.subtitle}</h3>
-        </div>
-
-        <div class="card" onClick={this.handleClick}>
-        <div class="graphs">
-        <h2 class="card-title">{this.props.title}</h2>
-
-          <XYPlot xDomain={[0, 6]} yDomain={[0, 10]} width={300} height={150}>
-
-            {/* <VerticalGridLines /> */}
-
-
-
-            <XAxis  tickTotal ={7} tickFormat = {v => `${DAYS[v]}`} 
-              style={{
-              line: {stroke: 'black'},
-              ticks: {stroke: 'black'},
-              text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
-}}/>
-            <YAxis  style={{
-              line: {stroke: 'black'},
-              ticks: {stroke: 'black'},
-              text: {stroke: 'none', fill: '#6b6b76', fontWeight: 600}
-}}/>
-
+      <>
+        {
+          (this.props.data != "empty") ? (
+            <ReactCardFlip isFlipped={this.state.isFlipped} infinite>
+              <div class="card" onClick={this.handleClick}>
+                <h2 class="card-title">{this.props.title}</h2>
+                <p class="card-num">{this.props.num}</p>
+                <h3 class="card-subtitle">{this.props.subtitle}</h3>
+              </div>
   
-
-            <LineSeries
-              curve={'curveMonotoneX'}
-              color={'#ED3F3F'}
-              opacity = {1}
-              
-
-                data={[
-                  {x: 0, y: 4, y0: 0},
-                  {x: 1, y: 2, y0: 0},
-                  {x: 2, y: 2, y0: 0},
-                  {x: 3, y: 1, y0: 0},
-                  {x: 4, y: 3, y0: 0},
-                  {x: 5, y: 1, y0: 0},
-                  {x: 6, y: 3, y0: 0}
-
-                ]}
-              />
-
-            
-          </XYPlot>
-
-        </div>
-        </div>
-      </ReactCardFlip>  
+              <div class="card" onClick={this.handleClick}>
+                <div class="graphs">
+                  <h2 class="card-title">{this.props.title}</h2>
+                  <LineChart width={300} height={150} data={this.props.data}>
+                    <CartesianGrid strokeDasharray="10 10" />
+                    <XAxis dataKey="x" />
+                    <YAxis />
+                    <Line type="monotone" dataKey="y" stroke="#8884d8" />
+                  </LineChart>
+                </div>
+              </div>
+            </ReactCardFlip>
+          ) : (
+            <div class="card">
+              <h2 class="card-title">{this.props.title}</h2>
+              <p class="card-num">{this.props.num}</p>
+              <h3 class="card-subtitle">{this.props.subtitle}</h3>
+            </div>
+          )
+        }
+      </>
     );
   }
 }
@@ -125,14 +103,13 @@ export default class Dash extends Component {
       prodAvg: 0,
       relAvg: 0,
       stressAvg: 0,
-    }
+    };
   }
 
-  signOut(){
+  signOut() {
     try {
-      authentication.signOut()
+      authentication.signOut();
       return <Redirect to="/" />;
-
     } catch (error) {
       alert(error);
     }
@@ -140,92 +117,159 @@ export default class Dash extends Component {
 
   componentDidMount() {
     var uid = firebase.auth().currentUser.uid;
-    db.collection('users').doc(uid).get().then(e => {
-      // console.log(e.data());
-      let data = e.data().entries;
-      let len = Object.keys(data).length;
-
-      let relSum = 0;
-      let relData = [];
-
-      let overSum = 0;
-      let healthSum = 0;
-      let hapSum = 0;
-      let learnSum = 0;
-      let prodSum = 0;
-      let stressSum = 0;
-      Object.keys(data).forEach((key) => {
-        relSum += data[key].relationships;
-        // relData.append((key, data[key].relationships))
-
-        overSum += data[key].overall;
-        healthSum += data[key].healthy;
-        hapSum += data[key].happiness;
-        learnSum += data[key].learn;
-        prodSum += data[key].productive;
-        stressSum += data[key].stress;
+    db.collection("users")
+      .doc(uid)
+      .get()
+      .then((e) => {
+        // console.log(e.data());
+        let data = e.data().entries;
+        if (data) {
+          let len = Object.keys(data).length;
+          let relSum = 0;
+          let overSum = 0;
+          let healthSum = 0;
+          let hapSum = 0;
+          let learnSum = 0;
+          let prodSum = 0;
+          let stressSum = 0;
+  
+          let relData = [];
+          let overData = [];
+          let healthData = [];
+          let hapData = [];
+          let learnData = [];
+          let prodData = [];
+          let stressData = [];
+  
+          Object.keys(data).forEach((key) => {
+            relSum += parseFloat(data[key].relationships);
+            relData.push({x: key, y: parseFloat(data[key].relationships)});
+            
+            overSum += parseFloat(data[key].overall);
+            overData.push({x: key, y: parseFloat(data[key].overall)});
+            
+            healthSum += parseFloat(data[key].healthy);
+            healthData.push({x: key, y: parseFloat(data[key].healthy)});
+            
+            hapSum += parseFloat(data[key].happiness);
+            hapData.push({x: key, y: parseFloat(data[key].happiness)});
+            
+            learnSum += parseFloat(data[key].learn);
+            learnData.push({x: key, y: parseFloat(data[key].learn)});
+            
+            prodSum += parseFloat(data[key].productive);
+            prodData.push({x: key, y: parseFloat(data[key].productive)});
+            
+            stressSum += parseFloat(data[key].stress);
+            stressData.push({x: key, y: parseFloat(data[key].stress)});
+          });
+  
+          this.setState({
+            totalEntries: len,
+            hapAvg: Math.round((100 * hapSum) / len) / 100,
+            healthAvg: Math.round((100 * healthSum) / len) / 100,
+            learnAvg: Math.round((100 * learnSum) / len) / 100,
+            overallAvg: Math.round((100 * overSum) / len) / 100,
+            prodAvg: Math.round((100 * prodSum) / len) / 100,
+            relAvg: Math.round((100 * relSum) / len) / 100,
+            stressAvg: Math.round((100 * stressSum) / len) / 100,
+            
+            relData: relData,
+            overData: overData,
+            healthData: healthData,
+            hapData: hapData,
+            learnData: learnData,
+            prodData: prodData,
+            stressData: stressData,
+          });
+        }
       });
-
-      // Math.round(100*hapSum/len)/100;
-      this.setState({
-        totalEntries: len,
-        hapAvg: Math.round(100*hapSum/len)/100,
-        healthAvg: Math.round(100*healthSum/len)/100,
-        learnAvg: Math.round(100*learnSum/len)/100,
-        overallAvg: Math.round(100*overSum/len)/100,
-        prodAvg: Math.round(100*prodSum/len)/100,
-        relAvg: Math.round(100*relSum/len)/100,
-        stressAvg: Math.round(100*stressSum/len)/100,
-      })
-      console.log(this.state)
-    })
   }
 
-  
-  
-  
   render() {
-    let p = 5
-    let ha = 5
-    let he = 5
-    let te = "this is a test note"
+    let p = 5;
+    let ha = 5;
+    let he = 5;
+    let te = "this is a test note";
 
     // console.log(this.props.location.state)
-    if (typeof(this.props.location.state) == 'undefined' ){
-      p=5
-    }
-    else{
-      p = this.props.location.state.productive
-      ha = this.props.location.state.happy
-      he = this.props.location.state.healthy
-      te = this.props.location.state.text
+    if (typeof this.props.location.state == "undefined") {
+      p = 5;
+    } else {
+      p = this.props.location.state.productive;
+      ha = this.props.location.state.happy;
+      he = this.props.location.state.healthy;
+      te = this.props.location.state.text;
     }
 
     return (
       <div class="full-dash">
-        <h1 class="dashTitle">{firebase.auth().currentUser.displayName.substr(0,firebase.auth().currentUser.displayName.indexOf(' '))}'s Dashboard </h1>
-        <button class="logout" onClick={this.signOut}>logout</button>
+        <h1 class="dashTitle">
+          {firebase
+            .auth()
+            .currentUser.displayName.substr(
+              0,
+              firebase.auth().currentUser.displayName.indexOf(" ")
+            )}
+          's Dashboard{" "}
+        </h1>
+        <button class="logout" onClick={this.signOut}>
+          logout
+        </button>
 
         <h2 class="subtitle">Stats</h2>
         <div class="card-cont">
-          <Card title="🔥  TOTAL" num = {this.state.totalEntries} subtitle="days tracking your life metrics" />
-          <Card title="😃  HAPPINESS" num={this.state.hapAvg} subtitle="overall" />
-          <Card title="💪  HEALTH" num={this.state.healthAvg} subtitle="overall" />
-          <Card title="📋  PRODUCTIVITY" num={this.state.prodAvg} subtitle="overall" />
-          <Card title="📚  LEARNING" num={this.state.learnAvg} subtitle="overall" />
-          <Card title="🏝  MANAGING STRESS" num={this.state.stressAvg} subtitle="overall" />
-          <Card title="✍🤝  RELATIONSHIPS" num={this.state.relAvg} subtitle="overall" />
+          <Card
+            title="🔥  TOTAL"
+            num={this.state.totalEntries}
+            subtitle="days tracking your life metrics"
+            data="empty"
+          />
+          <Card
+            title="😃  HAPPINESS"
+            num={this.state.hapAvg}
+            subtitle="overall"
+            data={this.state.hapData}
+          />
+          <Card
+            title="💪  HEALTH"
+            num={this.state.healthAvg}
+            subtitle="overall"
+            data={this.state.healthData}
+          />
+          <Card
+            title="📋  PRODUCTIVITY"
+            num={this.state.prodAvg}
+            subtitle="overall"
+            data={this.state.prodData}
+          />
+          <Card
+            title="📚  LEARNING"
+            num={this.state.learnAvg}
+            subtitle="overall"
+            data={this.state.learnData}
+            />
+          <Card
+            title="🏝  MANAGING STRESS"
+            num={this.state.stressAvg}
+            subtitle="overall"
+            data={this.state.stressData}
+            />
+          <Card
+            title="🤝  RELATIONSHIPS"
+            num={this.state.relAvg}
+            subtitle="overall"
+            data={this.state.relData}
+          />
         </div>
 
         <h2 class="subtitle">Journal Entries</h2>
         <Note title="this is a test note" />
-        <Note title={te}/>
+        <Note title={te} />
 
         <div>
           <Link to="/form">
-            <div Class="plus-button">
-              +
-            </div>
+            <div Class="plus-button">+</div>
           </Link>
         </div>
       </div>
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,155 +1,220 @@
-import React, { Component } from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
-import '../style.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "../style.css";
 import "firebase/firestore";
-import firebase from 'firebase';
-import app from "./services/firebase.js"
+import firebase from "firebase";
+import app from "./services/firebase.js";
 
 export default class Form extends Component {
-    constructor(props) {
-        var today = new Date(),
-        date = ((today.getMonth() + 1) + '-' + today.getDate() + '-' +  today.getFullYear()).toString()
-        super(props)
-        this.state = {
-            currentDate: date,
-            overall: 5,
-            healthy: 5,
-            productive: 5,
-            stress: 5,
-            relationships: 5,
-            learn: 5,
-            happy: 5,
-            text: ""
-        }
+  constructor(props) {
+    var today = new Date(),
+      date = (
+        today.getMonth() +
+        1 +
+        "-" +
+        today.getDate() +
+        "-" +
+        today.getFullYear()
+      ).toString();
+    super(props);
+    this.state = {
+      currentDate: date,
+      overall: 5,
+      healthy: 5,
+      productive: 5,
+      stress: 5,
+      relationships: 5,
+      learn: 5,
+      happy: 5,
+      text: "",
+    };
 
-        this.handleOverall = this.handleOverall.bind(this);
-        this.handleHealthy = this.handleHealthy.bind(this);
-        this.handleProductive = this.handleProductive.bind(this);
-        this.handleStress = this.handleStress.bind(this);
-        this.handleRelationships = this.handleRelationships.bind(this);
-        this.handleLearn = this.handleLearn.bind(this);
-        this.handleHappy = this.handleHappy.bind(this);
-        this.uploadData = this.uploadData.bind(this);
-
-    }
-
-    handleHealthy(event) {
-        this.setState({healthy: event.target.value});
-    }
-
-    handleOverall(event) {
-        this.setState({overall: event.target.value});
-    }
-    
-    handleProductive(event) {
-        this.setState({productive: event.target.value});
-    }
-
-    handleStress(event) {
-        this.setState({stress: event.target.value});
-    }
-
-    handleRelationships(event) {
-        this.setState({relationships: event.target.value});
-    }
-
-    handleLearn(event) {
-        this.setState({learn: event.target.value});
-    }
-
-    handleHappy(event) {
-        this.setState({happy: event.target.value});
-    }
-    
-    uploadData(){
-        let user = app.auth().currentUser.uid;
-        const db = firebase.firestore()
-        let state = this.state
-        let jsonObject = JSON.stringify({state})
-        console.log(jsonObject.state)
-        let date = this.state.currentDate
-        console.log(user,this.state.currentDate)
-
-        db.collection('users').doc(user).set({
-
-            entries :{
-                [date]: (state)
-                
-            }
-
-        },{ merge: true })
-
-            
-    }
-
-
-    render() {
-      let { overall, healthy, productive, stress, relationships, learn, happy } = this.state;
-
-      return (
-        <div>
-            <h1 class="formTitle">
-                Rate My Day
-            </h1>
-
-            <div class = 'date'>
-                <p>{ this.state.currentDate }</p>
-            </div>
-
-            <h3 class="formQuestion">How was your day overall? {overall}</h3>
-            <div class="slidecontainer">
-                <input type="range" min="0" max="10" value={overall} onChange={this.handleOverall} class="slider"></input>
-            </div>
-
-            <h3 class="formQuestion">How well did you practice healthy habits? {healthy}</h3>
-            <div class="slidecontainer">
-                <input type="range" min="0" max="10" value={healthy} onChange={this.handleHealthy} class="slider"></input>
-            </div>
-
-            <h3 class="formQuestion">How productive were you? {productive}</h3>
-            <div class="slidecontainer">
-                <input type="range" min="0" max="10" value={productive} onChange={this.handleProductive} class="slider"></input>
-            </div>
-
-            <h3 class="formQuestion">How well did you handle stress? {stress}</h3>
-            <div class="slidecontainer">
-                <input type="range" min="0" max="10" value={stress} onChange={this.handleStress} class="slider"></input>
-            </div>
-
-            <h3 class="formQuestion">How well did you manage your relationships? {relationships}</h3>
-            <div class="slidecontainer">
-                <input type="range" min="0" max="10" value={relationships} onChange={this.handleRelationships} class="slider"></input>
-            </div>
-
-            <h3 class="formQuestion">How much did you learn? {learn}</h3>
-            <div class="slidecontainer">
-                <input type="range" min="0" max="10" value={learn} onChange={this.handleLearn} class="slider"></input>
-            </div>
-
-            <h3 class="formQuestion">How happy were you? {happy}</h3>
-            <div class="slidecontainer">
-                <input type="range" min="0" max="10" value={happy} onChange={this.handleHappy} class="slider"></input>
-            </div>
-
-            <div class="journalContainer">
-                <textarea placeholder="Today I ... " class="longInput" cols="30" rows="10" ></textarea>
-            </div>
-
-            <div class="buttonContainer">
-                <Link to="/dashboard">
-                    <button class="backButton">Back</button>
-                </Link>
-                <Link to="/dashboard">
-                    <button onClick = {this.uploadData} class="submitButton">Submit</button>
-                </Link>
-            </div>
-
-        </div>
-      )
-    }
+    this.handleOverall = this.handleOverall.bind(this);
+    this.handleHealthy = this.handleHealthy.bind(this);
+    this.handleProductive = this.handleProductive.bind(this);
+    this.handleStress = this.handleStress.bind(this);
+    this.handleRelationships = this.handleRelationships.bind(this);
+    this.handleLearn = this.handleLearn.bind(this);
+    this.handleHappy = this.handleHappy.bind(this);
+    this.uploadData = this.uploadData.bind(this);
   }
+
+  handleHealthy(event) {
+    this.setState({ healthy: event.target.value });
+  }
+
+  handleOverall(event) {
+    this.setState({ overall: event.target.value });
+  }
+
+  handleProductive(event) {
+    this.setState({ productive: event.target.value });
+  }
+
+  handleStress(event) {
+    this.setState({ stress: event.target.value });
+  }
+
+  handleRelationships(event) {
+    this.setState({ relationships: event.target.value });
+  }
+
+  handleLearn(event) {
+    this.setState({ learn: event.target.value });
+  }
+
+  handleHappy(event) {
+    this.setState({ happy: event.target.value });
+  }
+
+  uploadData() {
+    let user = app.auth().currentUser.uid;
+    const db = firebase.firestore();
+    let state = this.state;
+    let jsonObject = JSON.stringify({ state });
+    console.log(jsonObject.state);
+    let date = this.state.currentDate;
+    console.log(user, this.state.currentDate);
+
+    db.collection("users")
+      .doc(user)
+      .set(
+        {
+          entries: {
+            [date]: state,
+          },
+        },
+        { merge: true }
+      );
+  }
+
+  render() {
+    let {
+      overall,
+      healthy,
+      productive,
+      stress,
+      relationships,
+      learn,
+      happy,
+    } = this.state;
+
+    return (
+      <div>
+        <h1 class="formTitle">Rate My Day</h1>
+
+        <div class="date">
+          <p>{this.state.currentDate}</p>
+        </div>
+
+        <h3 class="formQuestion">How was your day overall? {overall}</h3>
+        <div class="slidecontainer">
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={overall}
+            onChange={this.handleOverall}
+            class="slider"
+          ></input>
+        </div>
+
+        <h3 class="formQuestion">
+          How well did you practice healthy habits? {healthy}
+        </h3>
+        <div class="slidecontainer">
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={healthy}
+            onChange={this.handleHealthy}
+            class="slider"
+          ></input>
+        </div>
+
+        <h3 class="formQuestion">How productive were you? {productive}</h3>
+        <div class="slidecontainer">
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={productive}
+            onChange={this.handleProductive}
+            class="slider"
+          ></input>
+        </div>
+
+        <h3 class="formQuestion">How well did you handle stress? {stress}</h3>
+        <div class="slidecontainer">
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={stress}
+            onChange={this.handleStress}
+            class="slider"
+          ></input>
+        </div>
+
+        <h3 class="formQuestion">
+          How well did you manage your relationships? {relationships}
+        </h3>
+        <div class="slidecontainer">
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={relationships}
+            onChange={this.handleRelationships}
+            class="slider"
+          ></input>
+        </div>
+
+        <h3 class="formQuestion">How much did you learn? {learn}</h3>
+        <div class="slidecontainer">
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={learn}
+            onChange={this.handleLearn}
+            class="slider"
+          ></input>
+        </div>
+
+        <h3 class="formQuestion">How happy were you? {happy}</h3>
+        <div class="slidecontainer">
+          <input
+            type="range"
+            min="0"
+            max="10"
+            value={happy}
+            onChange={this.handleHappy}
+            class="slider"
+          ></input>
+        </div>
+
+        <div class="journalContainer">
+          <textarea
+            placeholder="Today I ... "
+            class="longInput"
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+
+        <div class="buttonContainer">
+          <Link to="/dashboard">
+            <button class="backButton">Back</button>
+          </Link>
+          <Link to="/dashboard">
+            <button onClick={this.uploadData} class="submitButton">
+              Submit
+            </button>
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
