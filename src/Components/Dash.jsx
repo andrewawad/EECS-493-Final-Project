0b6d47799a1db76704c5b0abfda45dfb501ce973
anawad <anawad@umich.edu>
2020-12-04 13:08:33 -0600

    changed graph color

--- src/Components/Dash.jsx
+++ src/Components/Dash.jsx
@@ -61,7 +61,7 @@ class Card extends Component {
                     <CartesianGrid strokeDasharray="10 10" />
                     <XAxis dataKey="x" />
                     <YAxis />
-                    <Line type="monotone" dataKey="y" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="y" stroke="#ED3F3F" />
                   </LineChart>
                 </div>
               </div>
